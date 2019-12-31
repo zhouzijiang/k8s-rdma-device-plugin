93bb70aee0a00083a889c4428659c2a83eaaeb36
zhouzijiang <zhouzijiang@jd.com>
2019-12-31 16:29:32 +0800

    filtered out inactive rdma device
    Signed-off-by: zhouzijiang <zhouzijiang@jd.com>

--- ibverbs/device.go
+++ ibverbs/device.go
@@ -15,7 +15,10 @@ import (
 func IbvGetDeviceList() ([]IbvDevice, error) {
 	var ibvDevList []IbvDevice
 	var c_num C.int
+	var index C.uint8_t
 	var c_ptrdevice *C.struct_ibv_device
+	var c_deviceAttr C.struct_ibv_device_attr
+	var c_portAttr C.struct_ibv_port_attr
 
 	c_devList := C.ibv_get_device_list(&c_num)
 
@@ -25,23 +28,51 @@ func IbvGetDeviceList() ([]IbvDevice, error) {
 
 	ptrSize := unsafe.Sizeof(c_ptrdevice)
 	ptr := uintptr(unsafe.Pointer(c_devList))
-	for i := 0; i <= int(c_num); i++ {
+	for i := 0; i < int(c_num); i++ {
 		c_ptrdevice = *(**C.struct_ibv_device)(unsafe.Pointer(ptr))
 		if c_ptrdevice == nil {
 			break
 		}
 
-		c_device := *c_ptrdevice
-		device := IbvDevice{
-			Name:       C.GoString(&c_device.name[0]),
-			DevName:    C.GoString(&c_device.dev_name[0]),
-			DevPath:    C.GoString(&c_device.dev_path[0]),
-			IbvDevPath: C.GoString(&c_device.ibdev_path[0]),
+		c_prtIbCtx := C.ibv_open_device(c_ptrdevice)
+		if c_prtIbCtx == nil {
+			ptr += ptrSize
+			continue
 		}
-		ibvDevList = append(ibvDevList, device)
+
+		ret := C.ibv_query_device(c_prtIbCtx, &c_deviceAttr)
+		if ret < 0 {
+			C.ibv_close_device(c_prtIbCtx)
+			ptr += ptrSize
+			continue
+		}
+
+		c_device := *c_ptrdevice
+
+		for index = 1; index <= c_deviceAttr.phys_port_cnt; index++ {
+			ret = C.ibv_query_port(c_prtIbCtx, index, &c_portAttr)
+			if ret < 0 {
+				continue
+			}
+
+			if c_portAttr.state == C.IBV_PORT_ACTIVE {
+				device := IbvDevice{
+					Name:       C.GoString(&c_device.name[0]),
+					DevName:    C.GoString(&c_device.dev_name[0]),
+					DevPath:    C.GoString(&c_device.dev_path[0]),
+					IbvDevPath: C.GoString(&c_device.ibdev_path[0]),
+				}
+				ibvDevList = append(ibvDevList, device)
+				break
+			}
+		}
+
+		C.ibv_close_device(c_prtIbCtx)
+
 		ptr += ptrSize
 	}
 
 	C.ibv_free_device_list(c_devList)
+
 	return ibvDevList, nil
 }
